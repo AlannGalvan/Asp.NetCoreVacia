7b45e97184887ca2ef1849b8a75d192294454cde
Alan <alan_galvan97@hotmail.com>
2024-07-13 23:07:54 -0300

    Subida

---
+++ Api Core/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Api_Core;
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
